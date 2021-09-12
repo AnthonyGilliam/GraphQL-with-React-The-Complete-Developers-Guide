36ac9a5e6487fc59312274f0ebe50315f6384352
Anthony <anthony.gilliam@gmail.com>
2021-09-12 00:56:37 -0400

    6:35 Section 6: Clientside GraphQL - MongoLab Setup

---
+++ Lyrical-GraphQL/client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { HashRouter, Route } from "react-router-dom";
+
+const Root = () => {
+  return <div>Lyrical</div>
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);
---
+++ Lyrical-GraphQL/server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const models = require('./models');
+const {graphqlHTTP} = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const schema = require('./schema/schema');
+
+const app = express();
+
+// Replace with your mongoLab URI
+const MONGO_URI = 'mongodb://127.0.0.1:27017/lyricaldb';
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI');
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DB'))
+    .catch(error => console.error('Mongo Connection Error', error));
+
+app.use(bodyParser.json());
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
